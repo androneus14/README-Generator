363158aecc001540d351666271fce06a1e521bdb
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-02 18:15:25 +1000

    added license badge, link and section code.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = [];
+  if(license === 'Apache License 2.0') {
+    licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else if (license === 'ISC') {
+    licenseBadge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+  } else if (license === 'MIT') {
+    licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (license === 'Mozilla Public License 2.0') {
+    licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  } else {
+    licenseBadge = []
+  } return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = [];
+  if(license === 'Apache License 2.0') {
+    licenseLink = ''
+  } else if (license === 'ISC') {
+    licenseLink = ''
+  } else if (license === 'MIT') {
+    licenseLink = ''
+  } else if (license === 'Mozilla Public License 2.0') {
+    licenseLink = ''
+  } return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = [];
+  if(license === 'Unlicensed') {
+    licenseSection = []
+  } else { 
+    licenseSection = 
+    `License: ${license}`
+  } return licenseSection
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
